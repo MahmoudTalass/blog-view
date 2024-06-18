7f6222a5e31e95566ef7dcc9b2b7ecdc3346372e
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-18 19:30:26 -0400

    Conditionally display login/logout and signup btns
    Depending on user authentication status.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
 
 function Navbar() {
+   const { token, setToken, setUser } = useAuth();
+
+   function logout() {
+      setToken(null);
+      setUser(null);
+      localStorage.removeItem("token");
+   }
+
    return (
-      <header className="flex justify-between p-6">
-         <h1 className="text-4xl">Blog View</h1>
-         <nav className="flex gap-2 items-center">
-            <Link to="login">Login</Link>
-            <Link to="signup">Sign up</Link>
-            <button>Logout</button>
+      <header className="flex justify-between p-6 mb-16">
+         <Link to="/">
+            <h1 className="text-4xl">Blog View</h1>
+         </Link>
+         <nav className="flex gap-6 items-center">
+            {token ? (
+               <button onClick={logout}>Logout</button>
+            ) : (
+               <>
+                  <Link to="login">Login</Link>
+                  <Link to="signup">Sign up</Link>
+               </>
+            )}
          </nav>
       </header>
    );
