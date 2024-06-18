954d91476c66908b4992dae774806d6d07fb0164
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-18 19:33:27 -0400

    Create sign up form for signing up and authentication

---
+++ src/components/Signup.jsx
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import InputField from "./InputField";
+import AuthenticationForm from "./AuthenticationForm";
+import SubmitButton from "./SubmitButton";
+import FormErrors from "./FormErrors";
+
+function Signup() {
+   const { user, setUser, token, setToken } = useAuth();
+   const [email, setEmail] = useState("");
+   const [name, setName] = useState("");
+   const [password, setPassword] = useState("");
+   const [error, setError] = useState(null);
+
+   async function handleSubmitSignup(e) {
+      e.preventDefault();
+      setPassword("");
+
+      try {
+         const response = await fetch("http://localhost:3000/api/auth/signup", {
+            mode: "cors",
+            method: "post",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, name, password, isAuthor: false }),
+         });
+
+         const json = await response.json();
+         if (!response.ok) {
+            setError(json.error);
+            setToken(null);
+            setUser(null);
+            return;
+         }
+
+         setToken(json.token);
+         setUser(json.user);
+         localStorage.setItem("token", json.token);
+         setPassword("");
+         setError(null);
+      } catch (err) {
+         setError({ message: "An error occurred. please try again later" });
+      }
+   }
+
+   console.log("user:", user, "token:", token);
+   if (token) {
+      return <Navigate to="/" />;
+   }
+
+   return (
+      <AuthenticationForm handleSubmit={handleSubmitSignup}>
+         <h1 className="text-3xl">Sign up Form</h1>
+         <InputField
+            type="text"
+            label="Name"
+            id="name"
+            value={name}
+            setValue={setName}
+            isRequired={true}
+            name="name"
+         />
+         <InputField
+            type="email"
+            label="Email"
+            id="email"
+            value={email}
+            setValue={setEmail}
+            isRequired={true}
+            name="email"
+         />
+         <InputField
+            type="password"
+            label="Password"
+            id="password"
+            value={password}
+            setValue={setPassword}
+            isRequired={true}
+            name="password"
+         />
+         <p>
+            Existing user?{" "}
+            <Link to="/login" className="underline">
+               Login here
+            </Link>
+         </p>
+         <SubmitButton>Sign up</SubmitButton>
+         {error && <FormErrors {...error} />}
+      </AuthenticationForm>
+   );
+}
+
+export default Signup;
