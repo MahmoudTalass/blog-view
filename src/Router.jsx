ba7a025f89c66073d935b551cd7b572e4c27948e
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-18 19:26:24 -0400

    Wrap entire app in AuthProvider layout

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,31 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Post from "./components/Post";
 import Home from "./components/Home";
 import MainPageLayout from "./components/MainPageLayout";
+import AuthProvider from "./components/contexts/AuthContext";
 
 export const router = createBrowserRouter([
    {
       path: "/",
-      element: <MainPageLayout />,
+      element: <AuthProvider />,
       children: [
          {
-            index: true,
-            element: <Home />,
+            element: <MainPageLayout />,
+            children: [
+               {
+                  index: true,
+                  element: <Home />,
+               },
+               {
+                  path: "post/:postId",
+                  element: <Post />,
+               },
+            ],
          },
          {
-            path: "post/:postId",
-            element: <Post />,
+            path: "login",
+            element: <Login />,
+         },
+         {
+            path: "signup",
+            element: <Signup />,
          },
       ],
-   },
-   {
-      path: "login",
-      element: <Login />,
-   },
-   {
-      path: "signup",
-      element: <Signup />,
    },
 ]);
