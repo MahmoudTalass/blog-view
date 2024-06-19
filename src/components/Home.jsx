4454db8a0d136395b23d52d341c225785d3dfc65
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-19 19:27:46 -0400

    Create home component to display post previews
    User can access posts through this component by clicking on the post.
    if they're authenticated they will be able to access it.

---
+++ src/components/Home.jsx
@@ -0,0 +1,66 @@
+import PostCard from "./PostCard";
+import { useState, useEffect } from "react";
+function Home() {
+   const [data, setData] = useState(null);
+   const [error, setError] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
+
+   useEffect(() => {
+      let active = true;
+
+      async function fetchData() {
+         try {
+            const response = await fetch("http://localhost:3000/api/posts");
+            const json = await response.json();
+
+            if (!response.ok) {
+               setData(null);
+               setError(json);
+               return;
+            }
+
+            if (active) {
+               setData(json);
+            }
+         } catch (err) {
+            setError(err);
+         } finally {
+            setIsLoading(false);
+         }
+      }
+
+      fetchData();
+
+      return () => {
+         active = false;
+      };
+   }, []);
+
+   if (isLoading) {
+      return <>Loading...</>;
+   }
+
+   if (error) {
+      return (
+         <>
+            Error {error.message} {error.status}
+         </>
+      );
+   }
+
+   return (
+      <div className="max-w-1200px">
+         <section className="flex flex-wrap w-full gap-5 p-6 justify-center">
+            {data && data.length > 0 ? (
+               data.map((post) => {
+                  return <PostCard key={post._id} post={post} />;
+               })
+            ) : (
+               <p>No posts yet</p>
+            )}
+         </section>
+      </div>
+   );
+}
+
+export default Home;
