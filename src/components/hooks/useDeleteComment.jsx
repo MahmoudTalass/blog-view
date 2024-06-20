1ffcd19fd39231c44bf8b19e5e569b3c5c3ebfe1
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-20 17:25:03 -0400

    Create a hook to delete a comment

---
+++ src/components/hooks/useDeleteComment.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function useDeleteComment(commentsDispatch) {
+   const [error, setError] = useState(null);
+
+   async function handleDeleteComment(comment) {
+      try {
+         const storedToken = localStorage.getItem("token");
+
+         const response = await fetch("http://localhost:3000/api/comments/" + comment._id, {
+            method: "delete",
+            headers: {
+               "content-type": "application/json",
+               authorization: "Bearer " + storedToken,
+            },
+            body: JSON.stringify({ authorId: comment.author._id }),
+         });
+
+         if (!response.ok) {
+            const json = await response.json();
+            setError(json.error);
+            return;
+         }
+
+         commentsDispatch({ type: "delete", commentId: comment._id });
+         setError(null);
+      } catch (err) {
+         setError(err);
+      }
+   }
+
+   return [handleDeleteComment, error, setError];
+}
+
+export default useDeleteComment;
