f426ac5a51260c1c2d8c4b8262db32329fd2e8bf
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-19 20:12:28 -0400

    Decode jwt to get user id and use it to identify current user

--- src/components/contexts/AuthContext.jsx
+++ src/components/contexts/AuthContext.jsx
@@ -1,22 +1,27 @@
 import { createContext, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import PropTypes from "prop-types";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext({
    token: null,
+   userId: null,
 });
 
 function AuthProvider() {
    const [token, setToken] = useState(null);
+   const [userId, setUserId] = useState(null);
 
    useEffect(() => {
       const storedToken = localStorage.getItem("token");
+      const id = jwtDecode(storedToken).id;
 
       setToken(storedToken);
+      setUserId(id);
    }, []);
 
    return (
-      <AuthContext.Provider value={{ token, setToken }}>
+      <AuthContext.Provider value={{ token, setToken, userId, setUserId }}>
          <Outlet />
       </AuthContext.Provider>
    );
