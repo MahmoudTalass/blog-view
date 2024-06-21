86692a99741b0710646f97104913a47316b8aa1e
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-21 16:38:28 -0400

    Create hook to return a function to update a comment

---
+++ src/components/hooks/useUpdateComment.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import useLogout from "./useLogout";
+
+function useUpdateComment(commentsDispatch) {
+   const [error, setError] = useState(null);
+   const logout = useLogout();
+
+   async function handleUpdateComment(commentId, authorId, text) {
+      try {
+         const storedToken = localStorage.getItem("token");
+
+         const body = {
+            authorId,
+            text,
+         };
+         const response = await fetch("http://localhost:3000/api/comments/" + commentId, {
+            method: "put",
+            headers: {
+               "content-type": "application/json",
+               authorization: "Bearer " + storedToken,
+            },
+            body: JSON.stringify(body),
+         });
+
+         let json;
+
+         if (response.status === 401) {
+            json = { error: { status: response.status, message: "Authentication required" } };
+            logout();
+         } else {
+            json = await response.json();
+         }
+
+         if (!response.ok) {
+            setError(json.error);
+            return;
+         }
+         commentsDispatch({ type: "update", comment: json });
+      } catch (err) {
+         setError(err);
+      }
+   }
+
+   return [handleUpdateComment, error, setError];
+}
+
+export default useUpdateComment;
