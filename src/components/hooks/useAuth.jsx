afad4ae559fa99305dbbf8ee85faf9d178de6751
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-21 16:41:38 -0400

    Export hook on different line

--- src/components/hooks/useAuth.jsx
+++ src/components/hooks/useAuth.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
-export default function useAuth() {
+function useAuth() {
    const context = useContext(AuthContext);
 
    return context;
 }
+
+export default useAuth;
