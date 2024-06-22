6cbdcce8c86b3634df893379edf71aaeee31e0b0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-22 11:09:43 -0400

    Hide error shortly after it happens

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Comment from "./Comment";
 import { useParams } from "react-router-dom";
 import useAuth from "./hooks/useAuth";
@@ -6,10 +6,21 @@ import PropTypes from "prop-types";
 import usePostComment from "./hooks/usePostComment";
 
 function CommentSection({ comments, commentsDispatch }) {
-   const [commentText, setCommentText] = useState("");
+   const [commentInput, setCommentInput] = useState("");
    const { postId } = useParams();
    const { userId } = useAuth();
-   const [handlePostComment, error] = usePostComment(commentsDispatch);
+   const [postComment, error, setError] = usePostComment(commentsDispatch);
+
+   useEffect(() => {
+      const timeoutId = setTimeout(() => {
+         setError(null);
+      }, 4000);
+      return () => clearTimeout(timeoutId);
+   });
+
+   function handlePostComment() {
+      postComment(postId, commentInput, setCommentInput);
+   }
 
    return (
       <section>
@@ -17,37 +28,43 @@ function CommentSection({ comments, commentsDispatch }) {
          <p className="text-2xl py-4 font-bold">comments:</p>
          <hr />
          <br />
-         <div className="flex gap-2 items-center mb-4">
-            <input
-               type="text"
-               placeholder="Add a comment..."
-               className="flex-grow bg-[#2C3E50] p-2 rounded"
-               value={commentText}
-               onChange={(e) => setCommentText(e.target.value)}
-            />
-            <button
-               disabled={commentText.length === 0}
-               className={`opacity-${
-                  commentText.length === 0 ? "10" : "100"
-               } bg-[#3498DB] py-2 px-5 rounded text-[#ECF0F1]`}
-               title="post"
-               onClick={() => handlePostComment(postId, commentText, setCommentText)}
-            >
-               Add comment
-            </button>
+         <div>
+            <div className="flex gap-2 items-center mb-4">
+               <input
+                  type="text"
+                  placeholder="Add a comment..."
+                  className="flex-grow bg-[#2C3E50] p-2 rounded"
+                  value={commentInput}
+                  onChange={(e) => setCommentInput(e.target.value)}
+               />
+               <button
+                  disabled={commentInput.length === 0}
+                  className={`opacity-${
+                     commentInput.length === 0 ? "10" : "100"
+                  } bg-[#3498DB] py-2 px-5 rounded text-[#ECF0F1]`}
+                  title="post"
+                  onClick={() => handlePostComment()}
+               >
+                  Add comment
+               </button>
+            </div>
+            {error && (
+               <div className="mb-5">
+                  {error.errors.length === 0 ? (
+                     <p className="text-red-600">{error.message}</p>
+                  ) : (
+                     error.errors.map((err) => {
+                        return (
+                           <p key={err} className="text-red-600">
+                              {err.message}
+                           </p>
+                        );
+                     })
+                  )}
+               </div>
+            )}
          </div>
-         {error &&
-            (error.errors.length === 0 ? (
-               <p className="text-red">{error.message}</p>
-            ) : (
-               error.errors.map((err) => {
-                  return (
-                     <p key={err} className="text-red">
-                        {err.message}
-                     </p>
-                  );
-               })
-            ))}
+
          <div className="flex flex-col gap-4">
             {comments.map((comment) => {
                return (
